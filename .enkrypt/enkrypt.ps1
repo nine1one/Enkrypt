e0e36a9128cececd51495204cf36036c34d1a1d2
иiɴᴇ[ᴏɴᴇ]ᴏɴᴇ <100479204+OneNiNE87@users.noreply.github.com>
2025-12-14 22:39:30 -0700

    Added Logic Checks for null bytes in the header.

--- .enkrypt/enkrypt.ps1
+++ .enkrypt/enkrypt.ps1
@@ -10,6 +10,22 @@ if (-not (Test-Path $InputFile)) {
     throw "Input file not found: $InputFile"
 }
 
+# Check if file is likely already encrypted (binary check)
+# We assume text files (Markdown) won't have null bytes in the first 1KB.
+try {
+    $Stream = [System.IO.File]::OpenRead($InputFile)
+    $HeaderBuffer = New-Object byte[] 1024
+    $ReadCount = $Stream.Read($HeaderBuffer, 0, 1024)
+}
+finally {
+    if ($Stream) { $Stream.Dispose() }
+}
+
+if ($ReadCount -gt 0 -and ($HeaderBuffer[0..($ReadCount-1)] -contains 0)) {
+    Write-Warning "Skipping execution: '$InputFile' appears to be binary or already encrypted."
+    exit
+}
+
 $PlainBytes = [System.IO.File]::ReadAllBytes($InputFile)
 
 $ProtectedBytes = [System.Security.Cryptography.ProtectedData]::Protect(
